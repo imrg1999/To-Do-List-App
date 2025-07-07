8378285c6bfb40f4768c2df40470638e9fd25ec4
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-07-08 02:25:06 +0530

    Add files via upload
    Preventing default of form submission

--- App.jsx
+++ App.jsx
@@ -2,13 +2,17 @@ import React, {useState} from 'react'
 
 function App() {
 
-const [task, setTask] = useState("")
+const [task, setTask] = useState("");
+
+const submithandle = (e) => {
+e.preventDefault();
+}
 
   return (
     <>
     <h1 className='bg-blue-400 text-center font-bold p-1'>
       To Do List</h1>
-      <form action="" className="flex justify-center">
+      <form action="" className="flex justify-center" onSubmit={submithandle}>
         <input type="text" onChange={(e)=> {setTask(e.target.value)}} className='border-zinc-500 text-xl border-3 m-2 px-3 py-2' placeholder='Enter task.....'
         value={task}/>
         <button className='bg-blue-400 border-3 m-2 px-3 py-2'>Add</button>
