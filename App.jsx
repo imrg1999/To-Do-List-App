1b8f3b22ecf8738401eeeec5eac8b7a76c81ffc1
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-07-08 01:57:06 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+
+const [task, setTask] = useState("")
+
+  return (
+    <>
+    <h1 className='bg-blue-400 text-center font-bold p-1'>
+      To Do List</h1>
+      <form action="" className="flex justify-center">
+        <input type="text" className='border-zinc-500 text-xl border-3 m-2 px-3 py-2' placeholder='Enter task.....'
+        value={task}/>
+        <button className='bg-blue-400 border-3 m-2 px-3 py-2'>Add</button>
+      </form>
+      <hr />
+
+      <div></div>
+    </>
+  )
+}
+
+export default App
