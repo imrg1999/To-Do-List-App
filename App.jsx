8067c8aedab8dfa4267e01026920bd48ac765253
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-07-08 13:18:29 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -10,6 +10,11 @@ e.preventDefault();
 setMaintask([...maintask, task])
 setTask("");
 }
+const deleteHandler = (index) => {
+  const updatedTasks = maintask.filter((_, i) => i !== index);
+  setMaintask(updatedTasks);
+};
+
 let renderTask; 
 if(maintask.length === 0) 
   {renderTask = <h1>There are no tasks</h1>}
@@ -19,20 +24,19 @@ renderTask = maintask.map((todo,index) => {
     <div className='flex justify-between w-2/3 mx-auto'>
     <h4>{todo}</h4>
     </div>
-    <button style={{background: 'grey'}} className='px-2 py4 font-bold rounded' onClick={(index) => {deleteHandler(index)}}>
-      Delete</button>
+    <button style={{background: 'grey'}} className='px-2 py4 font-bold rounded' onClick={() => {deleteHandler(index)}}>
+      Delete🗑️</button>
   </li>
 })
-const deleteHandler = (index) => {
-  let copyTask = [...maintask];
-  copyTask.splice(index,1);
-  setMaintask(copyTask);
-}
+
 }
   return (
     <>
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-blue-800 flex items-center justify-center">
+      <div className="bg-white shadow-2xl rounded-xl p-8 max-w-xl">
+        <h1 className='text-center'>🔗React Mini Project</h1>
     <h1 className='bg-blue-400 text-center font-bold p-1 font-5xl'>
-      To Do List</h1>
+      📝To Do List</h1>
       <form action="" className="flex justify-center" onSubmit={submithandle}>
         <input type="text" onChange={(e)=> {setTask(e.target.value)}} className='border-zinc-500 text-xl border-2 m-2 px-3 py-2' placeholder='Enter task.....'
         value={task}/>
@@ -41,6 +45,8 @@ const deleteHandler = (index) => {
 
       <div className='p-2 bg-blue-300 text-center'>
         <ul>{renderTask}</ul>
+      </div>
+      </div>
       </div>
     </>
   )
